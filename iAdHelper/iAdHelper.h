cb9003c45427ba5ae9b28e62034d20c26c96ff4c
Sven Anders Robbestad <robbestad@gmail.com>
2014-09-02 17:24:32 +0200

    Update iAdHelper.h
    Added an option to display iAD on top or bottom of the screen

--- iAdHelper/iAdHelper.h
+++ iAdHelper/iAdHelper.h
@@ -1,19 +1,24 @@
 //
 //  iAdHelper.h
+//  Rocket Doge
 //
 //  Created by Sven Anders Robbestad on 23.02.14.
-//  Copyright (c) 2014 Sven Anders Robbestad. All rights reserved.
-//  See LICENSE
+//  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
 #import <iAd/iAd.h>
 #ifndef CC_DIRECTOR
 #import "cocos2d.h"
 #endif
-
 #ifndef IADHELPER_LOGGING
     #define IADHELPER_LOGGING 0
 #endif
+
+typedef enum bannerPositionTypes
+{
+    TOP,
+    BOTTOM,
+} bannerPosition;
 
 @interface iAdHelper : NSObject
 
@@ -22,9 +27,10 @@
  * Call it once on application start to initialize ads
  */
 +(id) sharedHelper;
++ (void)setBannerPosition:(bannerPosition)bannerPositionTypes;
++ (bannerPosition)getBannerPosition;
 
 
 @property (atomic, retain) ADBannerView* bannerView;
-
 
 @end
